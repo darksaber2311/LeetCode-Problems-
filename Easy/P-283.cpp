fe84a3a2e9415a0078b151363271beb342692ca3
Abhigyaan Singh <abhigyaansingh123@gmail.com>
2025-12-20 19:04:49 +0530

    Add files via upload

---
+++ Easy/P-283.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int idx = 0;
+        for (int x : nums) {
+            if (x != 0) nums[idx++] = x;
+        }
+        while (idx < nums.size()) {
+            nums[idx++] = 0;
+        }
+    }
+};
