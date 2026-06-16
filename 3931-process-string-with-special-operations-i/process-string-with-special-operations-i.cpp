54e34df76002b2771805a3d7eae3128746a8ab31
Abhigyaan Singh <abhigyaansingh123@gmail.com>
2026-06-16 10:28:43 +0530

    Time: 4 ms (63.64%) | Memory: 47.1 MB (7.20%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -1,10 +1,65 @@
 class Solution {
 public:
     string processStr(string s) {
-        vector<char> result;
+        //vector<char> result;
         string res = "";
         int size = 0;
         for(char x:s)
+        {
+            if(int(x)>=97&&int(x)<=122)
+            {
+                res+=x;
+            }
+            else if(!res.empty()&&x == '#')
+            {
+                string store = res;
+                res+=store;
+            }
+            else if(!res.empty()&&x == '%')
+            {
+                string store1 = res;
+                for(int i=0;i<res.size();i++)
+                {
+                    res[i] = store1[res.size()-1-i];
+                }
+            }
+            else if(!res.empty()&&x == '*')
+            {
+                res.erase(res.size()-1,1);
+            }
+
+        }
+        return res;
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /*  for(char x:s)
         {
             if(int(x)<=122&&int(x)>=97)
             {
@@ -32,6 +87,6 @@ public:
         {
             res+=c;
         }
-        return res;
+        return res;*/
     }
 };
