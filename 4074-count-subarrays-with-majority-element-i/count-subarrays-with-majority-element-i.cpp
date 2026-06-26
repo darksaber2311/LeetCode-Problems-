124b26752435873ae8d7e0bb9d6b0ef2da63b1bf
Abhigyaan Singh <abhigyaansingh123@gmail.com>
2026-06-26 10:52:24 +0530

    Time: 58 ms (22.29%) | Memory: 39.7 MB (41.56%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -10,10 +10,7 @@ public:
                 count_target++;
             }
         }
-        if(count_target<=1)
-        {
-            return count_target;
-        }
+       
         int window_size = 2*count_target-1;
         int count = 0;
 
