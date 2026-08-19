33b53f3dcf71c5d38125dc91a8273405f8ec3427
Abhigyaan Singh <abhigyaansingh123@gmail.com>
2026-08-19 22:38:10 +0530

    Time: 21 ms (46.10%) | Memory: 36 MB (60.74%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,8 +3,8 @@ public:
     bool hasNeighbour(vector<vector<int>>&mat,int x, int y)
     {
         if(x<0 || x>=mat.size() || y<0 || y>= mat[0].size()) return false;
-        if(mat[x][y] == 1) return true;
-        return false;
+        //if(mat[x][y] == 1) return true;
+        return true;
     }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int mn_dist = INT_MAX;
@@ -30,26 +30,26 @@ public:
             pair<int,int>curr = path_cost.front().first;
             int x = curr.first;
             int y = curr.second;
-            int prev_cost = path_cost.front().second;
+            //int prev_cost = path_cost.front().second;
             if(hasNeighbour(mat,x-1,y) && res[x-1][y] == INT_MAX)
             {
-                res[x-1][y] = min(res[x-1][y],prev_cost+1);
-                path_cost.push({{x-1,y},prev_cost+1});
+                res[x-1][y] = res[x][y] + 1;
+                path_cost.push({{x-1,y},res[x-1][y]});
             }
             if(hasNeighbour(mat,x+1,y) && res[x+1][y] == INT_MAX)
             {
-                res[x+1][y] = min(res[x+1][y],prev_cost+1);
-                path_cost.push({{x+1,y},prev_cost+1});
+                res[x+1][y] = res[x][y] +1;
+                path_cost.push({{x+1,y},res[x+1][y]});
             }
             if(hasNeighbour(mat,x,y-1) && res[x][y-1] == INT_MAX)
             {
-                res[x][y-1] = min(res[x][y-1],prev_cost+1);
-                path_cost.push({{x,y-1},prev_cost+1});
+                res[x][y-1] = res[x][y] + 1;
+                path_cost.push({{x,y-1},res[x][y-1]});
             }
             if(hasNeighbour(mat,x,y+1) && res[x][y+1] == INT_MAX)
             {
-                res[x][y+1] = min(res[x][y+1],prev_cost+1);
-                path_cost.push({{x,y+1},prev_cost+1});
+                res[x][y+1] = res[x][y] + 1;
+                path_cost.push({{x,y+1},res[x][y+1]});
             }
             path_cost.pop();
             //visited[curr.first][curr.second] = true;
