cc23a1b6d165295243be8eab69918eedf98933f2
Abhigyaan Singh <abhigyaansingh123@gmail.com>
2026-08-21 14:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (78.11%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -11,28 +11,42 @@
 class Solution {
 public:
     void reorderList(ListNode* &head) {
-        ListNode* pointer1 = head;
-        ListNode* pointer2 = head->next;
-        ListNode* curr = head;
-        while(curr->next!=nullptr && curr->next->next!=nullptr)
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=nullptr && fast->next!=nullptr)
         {
-            while(pointer2->next!=nullptr)
-            {
-                pointer2 = pointer2->next;
-                pointer1 = pointer1->next;
-            }
-            ListNode* temp = nullptr;
-            //pointer1->next = nullptr;
-            temp = curr->next;
-            curr->next = pointer2;
-            pointer2->next = temp;
-            pointer1->next = nullptr;
-            curr = temp;
-            pointer1 = curr;
-            pointer2 = curr->next;
+            fast = fast->next->next;
+            //ListNode* mid = slow;
+            slow = slow->next;
+        }
+        ListNode* list2 = slow->next;
+        //mid->next = nullptr;
+        slow->next = nullptr;
+        ListNode* curr = list2;
+        ListNode* prev = nullptr;
+       // ListNode* next = list2->next;
+        while(curr!=nullptr)
+        {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+         
 
         }
-        //return head;
+        ListNode* pointer2 = prev;
+        ListNode* pointer1 = head;
+        while(pointer2!=nullptr)
+        {
+            ListNode* temp = pointer1->next;
+            ListNode* temp2 = pointer2->next;
+            pointer1->next = pointer2;
+            pointer2->next = temp;
+            pointer2 = temp2;
+            pointer1 = temp;
+        }
+
     }
     ListNode* func(ListNode* &head)
     {
