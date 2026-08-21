011cfb83de8b8bae20f85c011a8d1db02b768587
Abhigyaan Singh <abhigyaansingh123@gmail.com>
2026-08-21 11:01:33 +0530

    Time: 12 ms (41.72%) | Memory: 11.9 MB (54.02%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -12,6 +12,7 @@ public:
     // two ways to solve problem
     // 1.Hashmaps to detect if a node is visited twice -- Time O(n) and Space(O(n))
     // 2.Fast and slow pointers -- Time O(n) and Space(O(1)) -- bettter solution
+    // this fast and slow pointer method is called floyd's cycle detection algo or floyd's algo
 
     ListNode* fast = head;
     ListNode* slow = head; 
