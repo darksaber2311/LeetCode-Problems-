04b8b1d6292ead4b22a8df41dc1e7ef1d72e58b1
Abhigyaan Singh <abhigyaansingh123@gmail.com>
2026-08-25 09:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (94.59%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int res = 0;
-        int max_multiple = k;
-        int min_multiple = INT_MAX;
         int checker = k;
         sort(nums.begin(),nums.end());
         for(int i = 0;i<nums.size();i++)
         {
             if(nums[i] == checker)
-            {
-                //max_multiple = max(max_multiple,nums[i]);
-                //min_multiple = min(min_multiple,nums[i]);
+            { 
                 checker+=k;
-
-
             }
         }
-        //if(min_multiple == k)return max_multiple + k;
+        
         return checker;
     }
 };
